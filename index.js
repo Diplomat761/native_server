b4f42fba80f02bb77dc8047a9df91d31fd57f7c5
Diplomat761 <l7693744@gmail.com>
2023-03-12 16:57:21 +0700

    movie, genre CRUD

--- index.js
+++ index.js
@@ -1,67 +1,58 @@
-import http from 'http';
+import express from 'express';
 import pkg from 'pg';
 const { Pool } = pkg;
 
 const pool = new Pool({
-    user: 'postgres',
+    user: "postgres",
     password: 'rootroot',
-    host: 'localhost',
-    port: 5432,
-    database: 'test'
-});
+    host: "localhost",
+    port: "5432",
+    database: "test"
+})
 
-const server = http.createServer(async (req, res) => {
-    const { method, url, headers } = req;
-    let body = [];
-    req.on('data', (chunk) => {
-        body.push(chunk);
-    }).on('end', async () => {
-        body = Buffer.concat(body).toString();
-        let statusCode = 404;
-        let response = {};
-        try {
-            if (method === 'GET' && url === '/genre') {
-                const genres = await pool.query('SELECT * FROM genres');
-                statusCode = 200;
-                response = genres.rows;
-            } else if (method === 'GET' && url.startsWith('/genre/')) {
-                const id = url.split('/')[2];
-                const genre = await pool.query('SELECT * FROM genres WHERE id = $1', [id]);
-                if (genre.rows.length === 1) {
-                    statusCode = 200;
-                    response = genre.rows[0];
-                }
-            } else if (method === 'POST' && url === '/genre') {
-                const { id, name } = JSON.parse(body);
-                const newGenre = await pool.query('INSERT INTO genres (id, name) VALUES ($1, $2) RETURNING *', [id, name]);
-                statusCode = 201;
-                response = newGenre.rows[0];
-            } else if (method === 'PUT' && url === '/genre') {
-                const { id, name } = JSON.parse(body);
-                const updatedGenre = await pool.query('UPDATE genres SET name = $1 WHERE id = $2 RETURNING *', [name, id]);
-                statusCode = 200;
-                response = updatedGenre.rows[0];
-            } else if (method === 'DELETE' && url.startsWith('/genre/')) {
-                const id = url.split('/')[2];
-                const deletedGenre = await pool.query('DELETE FROM genres WHERE id = $1 RETURNING *', [id]);
-                if (deletedGenre.rows.length === 1) {
-                    statusCode = 200;
-                    response = deletedGenre.rows[0];
-                }
-            }
-        } catch (err) {
-            statusCode = 500;
-            response = { error: err.message };
-        } finally {
-            res.writeHead(statusCode, {
-                'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin': '*'
-            });
-            res.end(JSON.stringify(response));
-        }
-    });
-});
+const app = express();
 
-server.listen(5555, () => {
+app.use(express.json())
+
+
+
+
+
+//-----Фильмы-----
+app.post('/movie', async (req, res) => {
+    const {id, title, release_year} = req.body
+    const newMovie = await pool.query('INSERT INTO movies (id, title, release_year) values ($1, $2, $3) RETURNING *',
+    [id, title, release_year])
+    res.json(newMovie.rows[0])
+})
+app.get('/movie', async (req, res) => {
+    const movies = await pool.query('SELECT * FROM movies')
+    res.json(movies.rows)
+})
+app.get('/movie/:id', async (req, res) => {
+    const id = req.params.id
+    const movie = await pool.query('SELECT * FROM movies where id = $1', [id])
+    res.json(movie.rows[0])
+})
+app.put('/movie', async (req, res) => {
+    const {id, title, release_year} = req.body
+    const movie = await pool.query(`UPDATE movies set release_year = $3, title = $2 where id = $1 RETURNING *`,
+    [id, title, release_year])
+    res.json(movie.rows[0])
+})
+app.delete('/movie/:id', async (req, res) => {
+    const id = req.params.id
+    const movie = await pool.query('DELETE FROM movies where id = $1', [id])
+    res.json(movie.rows[0])
+})
+
+
+
+app.listen(5555, (err) => {
+    if (err) {
+        return console.log('ERRORRRRRRRR');
+    }
+
     console.log('Server OK');
 });
+
