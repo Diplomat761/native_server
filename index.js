aca9333f59a0b2ea4e5568eaecb78f509527f0d2
Diplomat761 <l7693744@gmail.com>
2023-03-12 22:33:39 +0700

    native movie_genres

--- index.js
+++ index.js
@@ -1,96 +1,77 @@
 import http from 'http';
+import url from 'url'
 import pkg from 'pg';
 const { Pool } = pkg;
 
 const pool = new Pool({
-    user: 'postgres',
+    user: "postgres",
     password: 'rootroot',
-    host: 'localhost',
-    port: 5432,
-    database: 'test'
-});
+    host: "localhost",
+    port: "5432",
+    database: "test"
+})
 
 const server = http.createServer(async (req, res) => {
-    const { method, url, headers } = req;
-    let body = [];
-    req.on('data', (chunk) => {
-        body.push(chunk);
-    }).on('end', async () => {
-        body = Buffer.concat(body).toString();
-        let statusCode = 404;
-        let response = {};
+    const { pathname, query } = url.parse(req.url, true);
+    const movieId = pathname.split('/')[2];
+    const genreId = pathname.split('/')[4];
+
+    if (req.method === 'POST' && pathname.match(/^\/movies\/\d+\/genres\/\d+$/)) {
         try {
-            if (method === 'GET' && url === '/movie') {
-                const movies = await pool.query('SELECT * FROM movies');
-                statusCode = 200;
-                response = movies.rows;
-            } else if (method === 'GET' && url.startsWith('/movie/')) {
-                const id = url.split('/')[2];
-                const movie = await pool.query('SELECT * FROM movies WHERE id = $1', [id]);
-                if (movie.rows.length === 1) {
-                    statusCode = 200;
-                    response = movie.rows[0];
-                }
-            } else if (method === 'POST' && url === '/movie') {
-                const { id, title, release_year } = JSON.parse(body);
-                const newMovie = await pool.query('INSERT INTO movies (id, title, release_year) VALUES ($1, $2, $3) RETURNING *', [id, title, release_year]);
-                statusCode = 201;
-                response = newMovie.rows[0];
-            } else if (method === 'PUT' && url === '/movie') {
-                const { id, title, release_year } = JSON.parse(body);
-                const updatedMovie = await pool.query('UPDATE movies SET title = $1, release_year = $2 WHERE id = $3 RETURNING *', [title, release_year, id]);
-                statusCode = 200;
-                response = updatedMovie.rows[0];
-            } else if (method === 'DELETE' && url.startsWith('/movie/')) {
-                const id = url.split('/')[2];
-                const deletedMovie = await pool.query('DELETE FROM movies WHERE id = $1 RETURNING *', [id]);
-                if (deletedMovie.rows.length === 1) {
-                    statusCode = 200;
-                    response = deletedMovie.rows[0];
-                }
-            } else if (method === 'GET' && url === '/genre') {
-                const genres = await pool.query('SELECT * FROM genres');
-                statusCode = 200;
-                response = genres.rows;
-            } else if (method === 'GET' && url.startsWith('/genre/')) {
-                const id = url.split('/')[2];
-                const genre = await pool.query('SELECT * FROM genres WHERE id = $1', [id]);
-                if (genre.rows.length === 1) {
-                    statusCode = 200;
-                    response = genre.rows[0];
-                }
-            } else if (method === 'POST' && url === '/genre') {
-                const { id, name } = JSON.parse(body);
-                const newGenre = await pool.query('INSERT INTO genres (id, name) VALUES ($1, $2) RETURNING *', [id, name]);
-                statusCode = 201;
-                response = newGenre.rows[0];
-            } else if (method === 'PUT' && url === '/genre') {
-                const { id, name } = JSON.parse(body);
-                const updatedGenre = await pool.query('UPDATE genres SET name = $1 WHERE id = $2 RETURNING *', [name, id]);
-                statusCode = 200;
-                response = updatedGenre.rows[0];
-            } else if (method === 'DELETE' && url.startsWith('/genre/')) {
-                const id = url.split('/')[2];
-                const deletedGenre = await pool.query('DELETE FROM genres WHERE id = $1 RETURNING *', [id]);
-                if (deletedGenre.rows.length === 1) {
-                    statusCode = 200;
-                    response = deletedGenre.rows[0];
-                }
-            }
+            const result = await pool.query(
+                'INSERT INTO movie_genres (movie_id, genre_id) values ($1, $2)',
+                [movieId, genreId]
+            );
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            res.write(`Жанр ${genreId} добавлен к фильму ${movieId}`);
+            res.end();
         } catch (err) {
-            statusCode = 500;
-            response = { error: err.message };
-        } finally {
-            res.writeHead(statusCode, {
-                'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin': '*'
-            });
-            res.end(JSON.stringify(response));
+            console.error(err);
+            res.writeHead(500, { 'Content-Type': 'text/plain' });
+            res.write('Ошибка при добавлении жанра к фильму');
+            res.end();
         }
-    });
+    } else if (req.method === 'GET' && pathname.match(/^\/movies\/\d+\/genres$/)) {
+        try {
+            const result = await pool.query(
+                'SELECT genres.name FROM genres JOIN movie_genres ON genres.id = movie_genres.genre_id WHERE movie_genres.movie_id = $1',
+                [movieId]
+            );
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.write(JSON.stringify(result.rows.map(row => row.name)));
+            res.end();
+        } catch (err) {
+            console.error(err);
+            res.writeHead(500, { 'Content-Type': 'text/plain' });
+            res.write('Ошибка при получении жанров к фильму');
+            res.end();
+        }
+    } else if (req.method === 'DELETE' && pathname.match(/^\/movies\/\d+\/genres\/\d+$/)) {
+        try {
+            const result = await pool.query(
+                'DELETE FROM movie_genres WHERE movie_id = $1 AND genre_id = $2',
+                [movieId, genreId]
+            );
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            res.write(`Жанр ${genreId} удален из фильма ${movieId}`);
+            res.end();
+        } catch (err) {
+            console.error(err);
+            res.writeHead(500, { 'Content-Type': 'text/plain' });
+            res.write('Ошибка при удалении жанра из фильма');
+            res.end();
+        }
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.write('Страница не найдена');
+        res.end();
+    }
 });
 
-server.listen(5555, () => {
+server.listen(5555, (err) => {
+    if (err) {
+        return console.log('ERRORRRRRRRR');
+    }
+
     console.log('Server OK');
 });
-
